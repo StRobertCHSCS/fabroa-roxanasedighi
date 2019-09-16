f4b42189aecbb57abf36defc43a8e690ef1451bb
roxanasedighi <roxana.sedighi22@ycdsbk12.ca>
2019-09-16 09:52:05 -0400

    example 1

---
+++ Working/ClassExamples/1_1.py
@@ -0,0 +1,9 @@
+print("This is a print statement")
+print("What's wrong with this statement?")
+print("How about this one?")
+print(5)
+print(3.14)
+print("Here are the ages of my three children: ", 12, 7, 4)
+colour = "black"
+print("His shoes are", colour)
+print("His pants are", colour)
