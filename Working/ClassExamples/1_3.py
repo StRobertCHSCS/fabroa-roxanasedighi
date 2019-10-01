b5b9c74c469b3c048adb950c29262dc8e9678533
roxanasedighi <roxana.sedighi22@ycdsbk12.ca>
2019-10-01 09:09:20 -0400

    new live hack files

--- Working/ClassExamples/1_3.py
+++ Working/ClassExamples/1_3.py
@@ -1,2 +1,4 @@
-name = input("Enter your name: ")
-print("Hello " + name)
+mark = 79
+raptor_pass = input("How many points to add for the raptor pass?")
+mark = mark + raptor_pass 
+print("mark: ", mark)
