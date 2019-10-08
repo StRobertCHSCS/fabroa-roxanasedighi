74b13475c041df32d5b55952d75a5f9f72de9e66
roxanasedighi <roxana.sedighi22@ycdsbk12.ca>
2019-10-08 09:42:03 -0400

    Microbit Tutorial

---
+++ Working/Microbit/buttons.py
@@ -0,0 +1,4 @@
+from microbit import *
+
+sleep(10000)
+display.scroll(str(button_a.get_presses()))
---
+++ Working/Microbit/hello.py
@@ -0,0 +1,5 @@
+from microbit import *
+
+while True:
+    display.scroll("Hello world!")
+    print("saying hello")
