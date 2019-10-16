c0063dee497be6d8959d986ef7699c6c73fe3c25
roxanasedighi <roxana.sedighi22@ycdsbk12.ca>
2019-10-16 09:20:31 -0400

    booleans exercises

---
+++ Working/Practice Questions/2_1_1_Booleans.py
@@ -0,0 +1,5 @@
+brought_food = True
+brought_drink = False
+
+print("Did the person bring food? " + str(brought_food))
+print("Did the person bring a drink? " + str(brought_drink))
---
+++ Working/Practice Questions/2_1_2_Booleans.py
@@ -0,0 +1,4 @@
+needs_water = True
+needs_to_be_repotted = False
+print("needs water:", needs_water)
+print("needs to be repotted:", needs_to_be_repotted)
