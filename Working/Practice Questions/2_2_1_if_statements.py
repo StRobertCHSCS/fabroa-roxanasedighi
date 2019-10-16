2e53810c2262caaa79ab92ddee2732dd147b3392
roxanasedighi <roxana.sedighi22@ycdsbk12.ca>
2019-10-16 09:21:12 -0400

    if statement exercises

---
+++ Working/Practice Questions/2_2_1_if_statements.py
@@ -0,0 +1,6 @@
+can_juggle = True
+
+if can_juggle:
+    print("I can juggle!")
+else:
+    print("I can't juggle.")
---
+++ Working/Practice Questions/2_2_2_if_statements.py
@@ -0,0 +1,9 @@
+is_raining = True
+
+print("Is it raining outside?")
+
+if is_raining:
+    print("Yes, I'm going to eat lunch inside!")
+
+else:
+    print("Nope, I'm going to eat lunch outside!")
