878e39f9bb8c3fff4392653c42aaded369e7f21e
roxanasedighi <roxana.sedighi22@ycdsbk12.ca>
2019-10-22 09:30:14 -0400

    comparison operators exercises

---
+++ Working/Practice Questions/2_3_6_Comparison_Operators.py
@@ -0,0 +1,7 @@
+reservation_name = "Shonda"
+name = input("Enter a name: ")
+
+if reservation_name == name: 
+    print("Right this way!")
+else:
+    print("Sorry, we don't have a reservation under that name.")
