93ed33a6b467489241b5e7806c6513e0e6980618
roxanasedighi <roxana.sedighi22@ycdsbk12.ca>
2019-11-04 09:39:37 -0500

    microbit logic assignment

--- Working/microbit_logic_assignment.py
+++ Working/microbit_logic_assignment.py
@@ -16,24 +16,20 @@ import music
 passive_buzzer = pin0
 moisture_sensor = pin1
 led_red = pin2
-micro_servo = pin3
 
 while True:
-    if moisture_value.write_digital() == 17:
+    if moisture_sensor.read_analog() == 17:
         print("Your plant is in good condition.")
-        music.play(music.JUMP_UP)
+        music.play(music.NYAN)
         display.show(Image.HAPPY)
 
-    elif button_a.is_pressed:
-         moisture_sensor >= 5 and moisture_sensor <= 12:
-            print("Water your plant every two days.")
-            music.play(music.JUMP_DOWN)
-            sensor.temperature()
+    elif button_a.is_pressed or 5 <= moisture_sensor <= 12:
+        print("Water your plant every two days.")
+        music.play(music.JUMP_DOWN)
+        display.scroll(temperature()) == 18
 
     else:
         print("you need to change the soil of the plant.")
         display.show(Image.SAD)
-        led_red.write_digital(0)
-        servo1.angle(45)   from microbit import *
-micro_servo = pin3
-micro_servo.write_angle(90)
+        led_red.write_digital(1)
+        
