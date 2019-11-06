d44adb0795a3d4d595a53f8dcd79938ff0d1079d
roxanasedighi <roxana.sedighi22@ycdsbk12.ca>
2019-11-06 09:13:53 -0500

    microbit logic assignment

--- Working/microbit_logic_assignment.py
+++ Working/microbit_logic_assignment.py
@@ -17,22 +17,22 @@ passive_buzzer = pin0
 moisture_sensor = pin1
 led_red = pin2
 
-#running pins
-pin0.write_analog(1)
-pin1.write_analog(1)
-pin2.write_analog(1)
+while True:
+    #running temeprature sensor
+    temp = temperature()
 
-if moisture_sensor.read_analog() > 17:
-    display.scroll("Your plant is in good condition.")
-    music.play(music.NYAN)
-    display.show(Image.HAPPY)
+    if moisture_sensor.read_analog() > 17:
+        display.scroll("Your plant is in good condition.")
+        music.play(music.NYAN)
+        display.show(Image.HAPPY)
 
-elif button_a.get_presses() or button_b.get_presses():
-    display.scroll("Water your plant every two days.")
-    music.play(music.JUMP_DOWN)
+    elif button_a.get_presses() or button_b.get_presses():
+        display.scroll("Water your plant every two days.")
+        display.scroll("The microbit temperature is" + str(temp) + "C.")
+        music.play(music.JUMP_DOWN)
 
-else:
-    display.scroll("you need to change the soil of the plant.")
-    led_red.write_digital(1)
-    display.show(Image.SAD)
-        
+    else:
+        display.scroll("you need to change the soil of the plant.")
+        music.play(music.WAWAWAWAA)
+        display.show(Image.SAD)
+        led_red.write_digital(1)
